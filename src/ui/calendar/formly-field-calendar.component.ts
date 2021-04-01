231d29d68a9c41577f4d195ebde50782f9ad65f5
Astrit Paci ATIS <astrit.paci@atis.al>
2021-04-01 10:22:15 +0200

    autocomplete and calendar fix

--- src/ui/calendar/formly-field-calendar.component.ts
+++ src/ui/calendar/formly-field-calendar.component.ts
@@ -1,20 +1,20 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {FieldType} from '@ngx-formly/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FieldType } from '@ngx-formly/core';
 
 @Component({
   selector: 'calendar-type',
   template: `
     <p-calendar
-      [defaultDate]="to.defaultDate"
-      [dateFormat]="to.dateFormat"
-      [hourFormat]="to.hourFormat"
-      [showTime]="to.showTime"
-      [showIcon]="to.showIcon"
-      [showButtonBar]="to.showButtonBar"
-      [showOtherMonths]="to.showOtherMonths"
-      [selectOtherMonths]="to.selectOtherMonths"
-      [selectionMode]="to.selectionMode || 'single'"
-      [numberOfMonths]="to.numberOfMonths || 1"
+      [defaultDate]="!!to.defaultDate ? to.defaultDate : null"
+      [dateFormat]="!!to.dateFormat ? to.dateFormat: 'mm/dd/yy'"
+      [hourFormat]="!!to.hourFormat ? to.hourFormat : '24'"
+      [showTime]="!!to.showTime ? to.hourFormat : false"
+      [showIcon]="!!to.showIcon ? to.showIcon : false"
+      [showButtonBar]="!!to.showButtonBar ? to.showButtonBar : false"
+      [showOtherMonths]="!!to.showOtherMonths ? to.showOtherMonths : false"
+      [selectOtherMonths]="!!to.selectOtherMonths ? to.selectOtherMonths : false"
+      [selectionMode]="!!to.selectionMode ? to.selectionMode : 'single'"
+      [numberOfMonths]="!!to.numberOfMonths ? to.numberOfMonths : '1'"
       [formControl]="formControl" [formlyAttributes]="field">
     </p-calendar>
   `,
